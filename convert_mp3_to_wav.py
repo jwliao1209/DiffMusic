8967de14a1134213f6bba26cc3b916d89791d6ea
Jia-Wei-Liao <sam23582211@gmail.com>
2024-12-12 19:27:39 +0800

    [add] music

--- convert_mp3_to_wav.py
+++ convert_mp3_to_wav.py
@@ -1,8 +1,8 @@
 from pydub import AudioSegment
 
 
-input_mp3 = "data/samples/f06eb04e688f5acb43716b1d4eb09b5c.mp3"
-output_wav = "data/samples/2.wav"
+input_mp3 = "data/samples/ec39eb2ef30e510d7d20db5146f3dfde.mp3"
+output_wav = "data/samples/5.wav"
 audio = AudioSegment.from_mp3(input_mp3)
 audio.export(output_wav, format="wav")
 
